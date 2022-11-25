505a9e42592e67b1daae638a8e03e7429d14e52f
ios <wow5702@naver.com>
2022-11-25 16:02:21 +0900

    Initial Commit

---
+++ SimpleMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by 이재웅 on 2022/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
